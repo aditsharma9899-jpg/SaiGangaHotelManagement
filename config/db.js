3d9062ce540976d85b9e7536ca2c9cdc3570982a
aditsharma9899-jpg <aditsharma9899@gmail.com>
2026-02-03 04:44:28 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect('mongodb+srv://aditsharma9899_db_user:1234567890@cluster0.edrio4b.mongodb.net/?appName=Cluster0');
 
     console.log("✅ MongoDB connected");
   } catch (error) {
