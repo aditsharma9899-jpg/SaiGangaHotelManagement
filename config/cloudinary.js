2d0f3bfc203019751cc8bc0e2897501b5c15b519
aditsharma9899-jpg <aditsharma9899@gmail.com>
2026-02-03 04:51:36 +0530

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: dx7fypwma,
-  api_key: 381472973261243,
-  api_secret: F5iJXPLp0dX0TsrwniFched9-EQ,
+  cloud_name: 'dx7fypwma',
+  api_key: '381472973261243',
+  api_secret: 'F5iJXPLp0dX0TsrwniFched9-EQ',
 });
 
 module.exports = cloudinary;
